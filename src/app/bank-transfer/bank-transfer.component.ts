aae17b81e80d0e7d2bb876beb2cf117955818371
Einbrodt <nicole.einbrodt@stud.th-luebeck.de>
2024-10-25 13:46:05 +0300

    designed bank transfer screen

--- src/app/bank-transfer/bank-transfer.component.ts
+++ src/app/bank-transfer/bank-transfer.component.ts
@@ -3,13 +3,16 @@ import {LucideAngularModule} from "lucide-angular";
 import {RouterLink} from "@angular/router";
 import {DemodataService} from "../shared/demodata.service";
 import {BankingOptions} from "../shared/banking-options";
+import {User} from "../shared/user";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-bank-transfer',
   standalone: true,
   imports: [
     LucideAngularModule,
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './bank-transfer.component.html',
   styleUrl: './bank-transfer.component.css'
@@ -17,10 +20,18 @@ import {BankingOptions} from "../shared/banking-options";
 export class BankTransferComponent {
   bankingOptions: BankingOptions[];
   currentOption: BankingOptions;
+  user: User;
+  amount: number;
 
   constructor(private demoService: DemodataService) {
     this.bankingOptions = this.demoService.getBankingOptions();
     this.currentOption = this.bankingOptions[0];
+    this.user = this.demoService.getUser();
+    this.amount = 10;
+  }
+
+  addAmount(amount: number) {
+    this.user.userBalance += amount;
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {LucideAngularModule} from "lucide-angular";
 import {RouterModule} from "@angular/router";
 import {User} from "../shared/user";
+import {DemodataService} from "../shared/demodata.service";
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,10 @@ import {User} from "../shared/user";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  user: User;
 
-  title = 'revClone';
-  userAccount = 'demo';
-  userPassword = '124534';
-  userFirstName = 'Nicole';
-  userBalance = 123.53;
-  constructor() {}
+  constructor(private demoService: DemodataService) {
+    this.user = this.demoService.getUser();
+
+  }
 }
